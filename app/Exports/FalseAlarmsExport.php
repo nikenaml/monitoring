425d80afd19e894819ffca12ae3847f7a544e480
Niken Amelia <niken.amelia123@gmail.com>
2021-12-12 12:16:08 +0700

    update fitur export excel heading

--- app/Exports/FalseAlarmsExport.php
+++ app/Exports/FalseAlarmsExport.php
@@ -4,9 +4,10 @@ namespace App\Exports;
 
 use App\Models\FalseAlarm;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 
-class FalseAlarmsExport implements FromCollection
+class FalseAlarmsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +15,21 @@ class FalseAlarmsExport implements FromCollection
     public function collection()
     {
         return FalseAlarm::all();
+    }
+
+    public function headings() :array
+    {
+        return [
+            "No",
+            "Tanggal Alert",
+            "Note Jumlah Alert per schedule",
+            "Total Alert All Schedule",
+            "Id Komentar Salah Prediksi",
+            "Jumlah Komentar Salah Prediksi",
+            "Persentase Salah Prediksi (%)",
+            "deleted_at",
+            "created_at",
+            "updated_at"
+        ];
     }
 }
