a81ec89e2730c7ee80a84f6ba750ce2bfe095ad9
Niken Amelia <niken.amelia123@gmail.com>
2022-01-09 17:45:46 +0700

    insert db and seed

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,12 +17,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create(
-            [
-            'name'=>'Admin1',
-            'email'=>'admin1sl@gmail.com',
-            'password'=> bcrypt('admin123'),
-            ]
-        );
+        try {
+            DB::table('users')->truncate(); //menghapus semua isi table users
+            DB::beginTransaction();
+            User::insert([
+                [
+                    'name'=>'Admin1',
+                    'email'=>'admin1sl@gmail.com',
+                    'password'=> bcrypt('admin123'),
+                    // 'created_at' => Carbon::now('Asia/Jakarta'),
+                    // 'updated_at' => Carbon::now('Asia/Jakarta')
+                ],
+                [
+                    'name'=>'Dev1',
+                    'email'=>'dev1sl@gmail.com',
+                    'password'=> bcrypt('dev123'),
+                    // 'created_at' => Carbon::now('Asia/Jakarta'),
+                    // 'updated_at' => Carbon::now('Asia/Jakarta')
+                ]]
+            );
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::info('seeder user => '.$th);
+        }
+        DB::commit();
     }
 }
